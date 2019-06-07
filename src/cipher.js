e3d64fd36d0726add1c483045c6f95e7670c66e7
shirleysosamuro <shirs.sosa@gmail.com>
2019-06-07 10:47:57 -0500

    shoutr al 35% login sirve, cifrar no sirve

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,19 @@
 window.cipher = {
   encode: (offset, string) => {
     /* Acá va tu código que cifra*/
-  },
-  decode: (offset, string) => {
-    /* Acá va tu código que descifra*/
-  }
-};
+    let solved = '';
+    for (let i =0;i< string.length;i++) {
+      const ascNum = string[i].charCodeAt();
+      if(ascNum>=65 && ascNum <=90);
+      const code = ((ascNum - 65) + (parseInt(offset))) % 26+ 65;
+      solved += String.fromCharCode(code);
+      return solved
+      }
+
+    }
+  };
+  
+  decode: (offset,string)=>{
+    /* Acá va tu código que cifra*/
+  };
+ 
--- src/index.js
+++ src/index.js
@@ -1,56 +1,71 @@
-//Nombrando las variables con sus equivalentes ID//
-const passwordValida ='LABORATORIA';
-const secondSection = document.getElementById('queHacer');
-const thirdSection =document.getElementById('writeMessage');
-const finalSection= document.getElementById('textoResultado');
+// VARIABLES PANTALLAS: login, primera pantalla con las opciones, segunda pantalla y tercera pantalla //
+const passwordValida = 'LABORATORIA';
+const primeraPantalla = document.getElementById('primeraPantallaOpciones');
+const segundaPantallaCifrar = document.getElementById('segundaPantallaOpcionCifrar');
+const segundaPantallaDescifrar = document.getElementById('segundaPantallaOpcionDesCifrar');
+const terceraPantallaResultadoFinal = document.getElementById('terceraPantallaResultados');
+const resultadoFinal=document.getElementById('enviarACifrar');
+
 //evento de login y verificación de clave//
-const login= document.getElementById('login');
-let nroIntentos= 0;
-const validar= () => {
-    const password= document.getElementById('secret');
-        if (password.value === passwordValida){
-            login.classList.add('hide');
-            secondSection.classList.replace('hide','show'); 
-//RECUERDA y agregar en style el GIF//
-            document.body.style.backgroundImage = 'url(./media/fondogif.GIF)';
-//RECUERDA que debo cambiar el console log por una looltip//
-        } else {
-        if (nroIntentos <2){
-           console.log('ERROR')
-           password.value= '';
-           nroIntentos++;
-//RECUERDA que debo cambiar el console log por una looltip ERROR. Prueba más tarde//
+const pantallaInicio = document.getElementById('login');
+let nroIntentos = 0;
+const validar = () => {
+  const password = document.getElementById('palabraSecreta');
+  if (password.value === passwordValida) {
+    login.classList.add('hide');
+    primeraPantalla.classList.replace('hide', 'show');
+    //RECUERDA y agregar en style el GIF//
+    document.body.style.backgroundImage =
+      'url(./media/fondo_shoutr_animado.gif)';
+    //RECUERDA que debo cambiar el console log por una looltip//
+  } else {
+    if (nroIntentos < 2) {
+      console.log('ERROR');
+      password.value = '';
+      nroIntentos++;
+      //RECUERDA que debo cambiar el console log por una looltip ERROR. Prueba más tarde//
     } else {
-           console.log('ERROR. Prueba más tarde'); }
- }  
-}   
-//evento dar click al botón y entrar a la secondSection //
-const botonLogin= document.getElementById('boton_entrar');
-botonLogin.addEventListener('click', validar)
-//dar click en botón OCULTAR y te mande a escribir mensaje  //
-const botonCifrar =document.getElementById('hideButton');
-      hideButton.addEventListener('click',()=> {
-      secondSection.classList.add('hide');
-      writeMessage.classList.replace('hide','show');
-    })
-//dar click en botón REVELAR y te mande a escribir mensaje  //
-const botonDescifrar =document.getElementById('discoverButton');
-      discoverButton.addEventListener('click',()=>{
-      secondSection.classList.add('hide');
-      writeMessage.classList.replace('hide','show');
-    })
-//dar click en botón enviar y te mande a resultado  //
-const botonEnviar= document.getElementById('codificar');
-      codificar.addEventListener('click',()=>{
-      writeMessage.classList.add('hide');
-      textoResultado.classList.replace('hide','show');      
-      })
-//ingresar el texto que se quiere cifrar o descifrar, elegir cantindad de desplazamientos y enviar información //
-//generar texto cifrado o descifrado//
-//funcion de copiar texto AUN NO ME SALE//
-function myFunction() {
-const copy =document.getElementById('cajaConResultado').value;
-copy.select();
-document.execCommand('copy');
-}
-//funcion de borrar datos//
+
+      console.log('Creeemos que no eres tú. Inténtalo más tarde.');   
+          
+    }
+  }
+};
+//evento dar click al botón y entrar a la primeraPantalla //
+const botonLogin = document.getElementById('botonEntrar');
+botonLogin.addEventListener('click', validar);
+
+//dar click en botón OCULTAR y te mande a segundaPantallaCifrar  //
+const botonCifrar = document.getElementById('opcionCifrar');
+opcionCifrar.addEventListener('click', () => {
+  primeraPantalla.classList.add('hide');
+  segundaPantallaCifrar.classList.replace('hide', 'show');
+});
+//¡NO ME SALE!dar click en botón REVELAR y te mande a segundaPantallaDescifrar   //
+const botonDescifrar = document.getElementById('opcionDescifrar');
+opcionDescifrar.addEventListener('click', () => {
+  primeraPantalla.classList.add('hide');
+  segundaPantallaDesCifrar.classList.replace('hide','show');
+});
+//dar click en botón enviarDESCIFRAR y te lleve a la pantalla final con el resultado  
+//const botonEnviarDescifrar = document.getElementById('enviarADescifrar');
+  //enviarADescifrar.addEventListener('click', () => {
+  //segundaPantallaOpcionDesCifrar.classList.add('hide');
+  //terceraPantallaResultados.classList.replace('hide', 'show')//
+
+  // CODIGO CIFRAR //
+  const resultadoCifrar= document.getElementById('enviarACifrar');
+  enviarACifrar.addEventListener('click', () => {
+  const mensajeCifrado = window.cipher.encode(textoParaCifrar.value,desplazamiento1.value);
+  cajaResultado.value = mensajeCifrado;  
+  });
+//(funcion de copiar texto AUN NO ME SALE//
+
+
+//funcion de volver a LOGIN y PONER  borrar datos//
+
+const regresarAInicio = document.getElementById('regresarLogin');
+  regresarLogin.addEventListener('click', () => {
+  terceraPantallaResultados.classList.add('hide');
+  login.classList.replace('hide', 'show');
+});
